596e6954531877288648e49b26bd8f31f4d0d514
Abel Dutra <abel.o.d@outlook.com>
2021-10-23 21:49:18 -0300

    feat: added auth tests

--- test/auth.spec.ts
+++ test/auth.spec.ts
@@ -17,4 +17,38 @@ test.group('Auth', () => {
 
     assert.exists(text)
   })
+
+  test('ensure authentication fails', async (assert) => {
+    const payload: LoginInterface = {
+      email: 'abel@email.com',
+      password: '123',
+    }
+
+    const obj = await supertest(BASE_URL).post('/session').send(payload).expect(400)
+
+    const { text } = obj
+
+    assert.exists(text)
+    assert.equal(
+      text,
+      JSON.stringify({ errors: [{ message: 'E_INVALID_AUTH_PASSWORD: Password mis-match' }] })
+    )
+  })
+
+  test('ensure authentication fails by email invalid', async (assert) => {
+    const payload: LoginInterface = {
+      email: 'abel@emaill.com',
+      password: '123',
+    }
+
+    const obj = await supertest(BASE_URL).post('/session').send(payload).expect(400)
+
+    const { text } = obj
+
+    assert.exists(text)
+    assert.equal(
+      text,
+      JSON.stringify({ errors: [{ message: 'E_INVALID_AUTH_UID: User not found' }] })
+    )
+  })
 })
